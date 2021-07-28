9b40b06b7be1daed5361cf8b7ccff45317ccda2f
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-07-28 12:28:39 -0700

    First commit

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+// const HelloWorld = (props) => (
+//   <div>
+//     <h2>Hello <em>{props.name}</em></h2>
+//   </div>
+// );
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello <em>{this.props.name}</em></h2>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
